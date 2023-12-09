757cb3d8e6d076a63a46bef95a41c26cda6a966d
NanoNish <nishkarsh1206@gmail.com>
2023-12-09 08:01:37 +0530

    feat: skeleton for abi wrapper

--- Transaction Methods/transaction.ts
+++ Transaction Methods/transaction.ts
@@ -1,18 +1,9 @@
-import Web3 from "web3";
+import ethers, { InterfaceAbi } from 'ethers';
 
-var web3 = new Web3('http://localhost:8545');
+export default async function txn(provider: ethers.JsonRpcProvider, address: string, options: Object, abi: InterfaceAbi) {
+    const chainId: bigint = (await provider.getNetwork()).chainId;
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(address, abi, provider);
 
-export async function init(provider: string) {
-    web3.setProvider(provider);
-}
-
-export async function sendTxn(from: string, options: any) {
-    web3.eth.sendTransaction({ from: from, ...options }).on('transactionHash', async (hash: string) => {
-        let chainId: bigint = await web3.eth.getChainId();
-        sendData(hash, chainId)
-    });
-}
-
-function sendData(hash: string, chainId: bigint) {
     
 }
